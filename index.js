a6beff5096f0d5086c114d215819faaae57f0296
jLuseno161 <joyluseno2@gmail.com>
2021-04-09 23:20:18 +0300

    formulae to calculate day of week born

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ window.onload = function () {
             const aMale = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
             const aFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
+            //find day of week born
+            if (month < 3) {
+                month += 12;
+                year--;
+            }
+            let index = (date + parseInt(((month + 1) * 26) / 10) + year + parseInt(year / 4) + 6 * parseInt(year / 100) + parseInt(year / 400) - 1) % 7;
+
         }
         dayBorn();
     });
