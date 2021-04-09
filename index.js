49bcd7e6b2e40ed9f168f716eda223c04d306184
jLuseno161 <joyluseno2@gmail.com>
2021-04-09 23:14:10 +0300

    fetch user input

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 window.onload = function () {
     document.getElementById('btn').addEventListener('click', function () {
         let dayBorn = function (DD, MM, YY) {
+
+            //fetch user input
+            YY = document.forms["findAkanName"]["inputYear"].value;
+            MM = document.forms["findAkanName"]["inputMonth"].value;
+            DD = document.forms["findAkanName"]["inputDay"].value;
+
         }
         dayBorn();
     });
